a3e7cf1c7e8b12d79a772b1809aa05be7673895e
Anton Limar <antonreal93@gmail.com>
2026-04-13 21:31:19 +0500

    Update example app dependencies

--- examples/with-redux/src/index.jsx
+++ examples/with-redux/src/index.jsx
@@ -1,13 +1,14 @@
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { createStore } from 'redux';
 import { reducer as app } from './reducer';
 
 import { Provider } from 'react-redux';
 import App from './App';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+
+createRoot(rootElement).render(
   <Provider store={createStore(app)}>
     <App />
   </Provider>,
-  document.getElementById('root'),
 );
